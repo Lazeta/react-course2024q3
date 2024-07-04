64a6fc6ab4c7059c344a9ff3a33b1e1352e0e23c
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-07-04 21:52:46 +0300

    feat: add first test class component and update configure file eslint; install dependency

---
+++ class-components/src/components/first_class.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+interface GreetingProps {
+    name: string;
+}
+
+export class Greeting extends React.Component<GreetingProps> {
+  render() {
+    return <h1>Hello, {this.props.name}!</h1>;
+  }
+}
+
+<Greeting name="Taylor" />
